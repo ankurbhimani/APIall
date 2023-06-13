af449e6055e7ee4ac07724625d12238c94eb2c46
ankurbhimani <ankurbhimani07@gmail.com>
2023-06-13 17:00:23 +0530

    add

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ require('./db/conection');
 app.listen(2016, () => {
     console.log('i am server');
 });
-// app.use('/', (req, res) => {
-//     return res.send("backend started")
-// });
+app.use('/', (req, res) => {
+    return res.send("backend started")
+});
 
 app.use('/api', require('./api/to_do'));
